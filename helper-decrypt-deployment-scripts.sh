a8112327c25e37e7265cd05ed78390ed9dc20282
Philip Southam <philip.southam@jpl.nasa.gov>
2020-03-22 12:28:51 -0700

    adding helper to decrypt deployment script

---
+++ helper-decrypt-deployment-scripts.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# In order to pass the appropriate (sometimes sensitive) paramenters to the CloudFormation templates I
+# use a set of deployment scripts. This file decrypts the files stored in git.
+for f in $(ls -1 deploy-???-*.sh.gpg); do
+    destfile="${f::(-4)}"
+    gpg --output "${destfile}" --decrypt "${f}"
+done
--- helper-encrypt-deployment-scripts.sh
+++ helper-encrypt-deployment-scripts.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # In order to pass the appropriate (sometimes sensitive) paramenters to the CloudFormation templates I
-# use a set of deployment scripts. This file encrypts those files so I can "safely" store them.
+# use a set of deployment scripts. This file encrypts those files so I can "safely" store them. If you want
+# access to decrypt these files please let me know.
 for f in $(ls -1 deploy-???-*.sh); do
     echo "${f}"
     rm --force "${f}.gpg"
