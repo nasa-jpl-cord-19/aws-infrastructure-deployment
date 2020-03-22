fb804e3cfcfb15c068a126d09bc52fe5923de8db
Philip Southam <philip.southam@jpl.nasa.gov>
2020-03-22 16:05:51 -0700

    Fixing up templates to share resources and identify themselves better

--- helper-encrypt-deployment-scripts.sh
+++ helper-encrypt-deployment-scripts.sh
@@ -7,5 +7,7 @@ for f in $(ls -1 deploy-???-*.sh); do
     rm --force "${f}.gpg"
     gpg --encrypt --sign\
         --recipient "philip.southam@jpl.nasa.gov"\
+        --recipient "03CC5FFA61AAAD3C8FF25E9270F09CC6B876884A"\
     "${f}"
+    git add "${f}.gpg"
 done
